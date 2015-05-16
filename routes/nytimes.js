7adbafbd1935dfe08386c979766eb5c08463c9aa
Mats Högberg <mats@hgbrg.se>
2015-05-16 13:30:00 +0200

    nytimes

--- routes/nytimes.js
+++ routes/nytimes.js
@@ -77,8 +77,8 @@ router.get('/', function(req, res, next) {
 					'snippet': current.snippet,
 					// 'type_of_material': current.type_of_material,
 					'time': current.pub_date,
-					'lat': place.lat + Math.random() / 1000,
-					'lng': place.lng + Math.random() / 1000
+					'lat': place.lat + Math.random() / 100,
+					'lng': place.lng + Math.random() / 100
 				});
 			});
 
