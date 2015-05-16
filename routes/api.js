6b6ef4b65621b7adfcd1f36ad6b874266ee01ca6
Mats Högberg <mats@hgbrg.se>
2015-05-16 08:37:26 +0200

    API

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET api listing. */
 router.get('/', function(req, res, next) {
@@ -9,8 +10,22 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
 	var lng = req.body.lng;
 	var lat = req.body.lat;
+	
+	var twitter = twitter(lng, lat);
 });
 
-
-
 module.exports = router;
+
+function twitter(lng, lat) {
+	var url = 'https://api.twitter.com/1.1/search/tweets.json?q=&geocode=' + lat + ',' + lng + ',1mi';
+	console.log('twitter url: ' + url);
+
+	var options;
+	options.url = url;
+	options.method = 'POST';
+
+	request(options, function(error, response, body) {
+		console.log('response: ' + response);
+		console.log('body: ' + body);
+	});
+}
